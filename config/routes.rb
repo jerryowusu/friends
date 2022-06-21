01342bc556b387d8f53a8288d846ddfff2f563a8
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-21 01:37:27 -0400

    Add devise for user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   get 'home/about'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
